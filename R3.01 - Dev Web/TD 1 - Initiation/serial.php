b4e157046dbb24c6ed26164eae31bc2fc443df6b
Pikoow <pikdev.gopi@gmail.com>
2024-09-05 10:33:51 +0200

    R3.01

---
+++ R3.01 - Dev Web/TD 1 - Initiation/serial.php
@@ -0,0 +1,13 @@
+<?php
+$ser_var1 = 123;
+$ser_var2 = 123.5;
+$ser_var3 = "Toto";
+$ser_var4 = [
+   "Lulu",
+   23
+];
+$ser_var5 = [
+   22300 => "Lannion",
+   22000 => "St Brieuc",
+   22200 => "Guingamp"
+];
