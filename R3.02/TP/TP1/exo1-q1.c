ff31b84da6d9c997a2c9161a1d166a9ca0fac8d5
Pikoow <pikdev.gopi@gmail.com>
2024-09-11 16:09:38 +0200

    R3.02

---
+++ R3.02/TP/TP1/exo1-q1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int x = 100;
+    int *pt = &x;
+
+    *pt += 10;
+
+    printf("Valeur pointée par pt : %d\n", *pt);
+    printf("Valeur de x : %d\n", x);
+
+    return 0;
+}
---
+++ R3.02/TP/TP1/exo1-q2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int *pt = (int*)malloc(sizeof(int));
+
+    if (pt == NULL) {
+        printf("Échec de l'allocation mémoire\n");
+        return 1;
+    }
+
+    *pt = 100;
+
+    printf("Valeur pointée par pt : %d\n", *pt);
+
+    return 0;
+}
