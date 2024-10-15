d38de3af7c18d19ee5677c07d15296f31d60c433
Pikoow <pikdev.gopi@gmail.com>
2024-10-15 11:08:43 +0200

    R3.01

---
+++ R3.01/DevJS/Partie2/exo1.js
@@ -0,0 +1,45 @@
+let $message = prompt("Quel mot voulez-vous traduire ?");
+
+$formattedMessage = $message.toLowerCase().trim();
+
+function louchebem($message) {
+    let $premiereLettre = $message.charAt(0).toLowerCase();
+
+    let $fin;
+
+    switch ($premiereLettre) {
+        case "b":
+            $fin = "em";
+            break;
+
+        case "c":
+            $fin = "ès";
+            break;
+
+        case "d":
+            $fin = "é";
+            break;
+
+        case "f":
+            $fin = "oque";
+            break;
+
+        case "m":
+            $fin = "uche";
+            break;
+
+        case "p":
+            $fin = "é";
+            break;
+
+        default:
+            $fin = "ès";
+            break;
+    }
+
+    $message = $message.substring(1, $message.length);
+
+    return `L${$message}${$premiereLettre}${$fin}`;
+}
+
+console.log(louchebem($formattedMessage));
