907d726c990992e2f0d01848608906b8a7cd7dfb
Pikoow <pikdev.gopi@gmail.com>
2024-11-28 12:31:15 +0100

    R3.05

---
+++ R3.05/TP_Socket/TP1/exo1.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define PORT 8080
+#define BACKLOG 1
+
+int main() {
+    int server_sock, client_sock;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+    int ret;
+
+    // Création
+    server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_sock == -1) {
+        perror("Erreur lors de la création du socket");
+        exit(EXIT_FAILURE);
+    }
+    printf("Socket créé avec succès.\n");
+
+    // Liaison à une adresse et un port
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");;
+    server_addr.sin_port = htons(PORT);
+
+    ret = bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    if (ret == -1) {
+        perror("Erreur lors de la liaison (bind)");
+        close(server_sock);
+        exit(EXIT_FAILURE);
+    }
+    printf("Socket lié à l'adresse et au port %d.\n", PORT);
+
+    // Mise en écoute
+    ret = listen(server_sock, BACKLOG);
+    if (ret == -1) {
+        perror("Erreur lors de la mise en écoute (listen)");
+        close(server_sock);
+        exit(EXIT_FAILURE);
+    }
+    printf("Serveur en attente de connexion sur le port %d...\n", PORT);
+
+    // Acceptation d'une connexion
+    client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &client_addr_len);
+    if (client_sock == -1) {
+        perror("Erreur lors de l'acceptation (accept)");
+        close(server_sock);
+        exit(EXIT_FAILURE);
+    }
+    printf("Connexion acceptée depuis %s:%d.\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+    // Fermeture
+    close(client_sock);
+    close(server_sock);
+    printf("Serveur fermé.\n");
+
+    return 0;
+}
---
+++ R3.05/TP_Socket/TP1/tp1.md
@@ -0,0 +1,14 @@
+# TP1
+
+Question 1 :
+
+- mkdir TP1
+- cd TP1
+- echo '<!DOCTYPE html><html><head><title>Test</title></head><body><h1>Hello world!</h1></body></html>' > doc.html'
+- php -S localhost:8080
+- <http://localhost:8080/doc.html>
+
+Question 2 :
+
+- telnet localhost 8080
+- GET /doc.html
