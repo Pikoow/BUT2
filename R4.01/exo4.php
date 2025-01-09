ef81c0fb2576df979b31ac779df5c4e6d78b51dc
Pikoow <pikdev.gopi@gmail.com>
2025-01-09 09:31:53 +0100

    R4.01

--- R4.01/exo4.php
+++ R4.01/exo4.php
@@ -12,42 +12,84 @@ $contenu = file_get_contents('https://restcountries.com/v3.1/all?fields=name,cca
 $decode = json_decode($contenu, true);
 
 ?>
-<form>
+
+<form action="exo4.php" method="POST" enctype="multipart/form-data">
     <label for="nom">Nom du pays</label>
     <input type="text" id="name" name="name" />
 
     <input type="submit" value="Chercher" />
 </form>
 
-<table>
-    <tr>
-        <td>Nom</td>
-        <td>CCA3</td>
-        <td>Capitale</td>
-    </tr>
 <?php
+if (isset($_POST["name"])) {
+    ?>
+        <table>
+            <tr>
+                <th>Nom</th>
+                <th>CCA3</th>
+                <th>Capitale</th>
+            </tr>
+            <tr>
+                <?php
+                for ($i=0; $i < count($decode); $i++) {
+                    if (str_contains(strtolower($decode[$i]["name"]["common"]), strtolower($_POST["name"]))) {
+                    ?>
+                        <td>
+                    <?php
+                        echo $decode[$i]["name"]["common"];
+                    ?>
+                        </td>
+                        <td>
+                    <?php
+                        echo $decode[$i]["cca3"];
+                    ?>
+                        </td>
+                        <td>
+                    <?php
+                        echo $decode[$i]["capital"]["0"];
+                    ?>
+                        </td>
+                        </tr>
+                    <?php
+                    }
+                }
+                ?>
+            </tr>
+    <?php
+} else {
+    ?>
+        <table>
+            <tr>
+                <td>Nom</td>
+                <td>CCA3</td>
+                <td>Capitale</td>
+            </tr>
+        <?php
 
-for ($i=0; $i < count($decode); $i++) {
-?>
-    <tr>
-    <td>
-<?php
-    echo $decode[$i]["name"]["common"];
-?>
-    </td>
-    <td>
-<?php
-    echo $decode[$i]["cca3"];
-?>
-    </td>
-    <td>
-<?php
-    echo $decode[$i]["capital"]["0"];
-?>
-    </td>
-    </tr>
-<?php
+        for ($i=0; $i < count($decode); $i++) {
+        ?>
+            <tr>
+            <td>
+        <?php
+            echo $decode[$i]["name"]["common"];
+        ?>
+            </td>
+            <td>
+        <?php
+            echo $decode[$i]["cca3"];
+        ?>
+            </td>
+            <td>
+        <?php
+            echo $decode[$i]["capital"]["0"];
+        ?>
+            </td>
+            </tr>
+        <?php
+        }
+
+        ?>
+        </table>
+    <?php
 }
-
 ?>
-</table>
---
+++ R4.01/exo5.php
@@ -0,0 +1,39 @@
+<?php
+
+stream_context_set_default([
+    'http' => [
+        'proxy' => '129.20.239.11:3128'
+    ]
+]);
+
+$listePays =  file_get_contents('https://restcountries.com/v3.1/all?fields=name,cca3,capital,flags');
+
+$decodeJson = json_decode($listePays, true);
+
+?>
+
+<table>
+    <tr>
+        <th>Nom du pays</th>
+        <th>CCA3</th>
+        <th>Capitale</th>
+        <th>Drapeau</th>
+    <tr>
+
+<?php
+
+for ($valeur = 0 ; $valeur < count($decodeJson) ; $valeur++) {
+
+?>
+
+    <tr>
+        <td><?php echo htmlentities($decodeJson[$valeur]['name']['common']) ?></td>
+        <td><?php echo htmlentities($decodeJson[$valeur]['cca3']) ?></td>
+        <td><?php echo htmlentities($decodeJson[$valeur]['capital']['0']) ?></td>
+        <td><img src="<?php echo htmlentities($decodeJson[$valeur]['flags']['png']) ?>"></td>
+    </tr>
+<?php
+
+}
+
+?>
